824b69429ba5676e0fc644f509407e02d914f0d1
Yunchen Xue <yunchenxue30@gmail.com>
2022-07-30 00:28:39 +0800

    loginTest Pass 🥵

--- src/Login.test.ts
+++ src/Login.test.ts
@@ -4,37 +4,38 @@ import { loginFunc } from './utils/login';
 // const fs = require('fs')
 const xlsx = require('node-xlsx')
 
-let loginTestData: LoginTestDataUnit[] = 
-[{
-  account: 'faqob@dropjar.com',
-  pwd: 'Xyc980830',
-  expectResult: '🎉 🎉 🎉 登录成功！',
-  type: 0
-},
-{
-  account: 'faqob@dropjar.com',
-  pwd: 'Xyc980831',
-  expectResult: '账号或密码错误',
-  type: 1
-},
-{
-  account: 'sawuhuw@givmail.com',
-  pwd: '1123456',
-  expectResult: '账号或密码错误',
-  type: 1
-},
-{
-  account: 'sawuhuw@givmail.com',
-  pwd: '2123456',
-  expectResult: '账号或密码错误',
-  type: 1
-},
-{
-  account: 'sawuhuw@givmail.com',
-  pwd: '123456',
-  expectResult: '🎉 🎉 🎉 登录成功！',
-  type: 0
-}];
+// todo: 配置文件的使用
+let loginTestData: LoginTestDataUnit[] = [
+  {
+    account: 'faqob@dropjar.com',
+    pwd: 'Xyc980830',
+    expectResult: '🎉 🎉 🎉 登录成功！',
+    type: 0
+  },
+  {
+    account: 'faqob@dropjar.com',
+    pwd: 'Xyc980831',
+    expectResult: '账号或密码错误',
+    type: 1
+  },
+  {
+    account: 'sawuhuw@givmail.com',
+    pwd: '1123456',
+    expectResult: '账号或密码错误',
+    type: 1
+  },
+  {
+    account: 'sawuhuw@givmail.com',
+    pwd: '2123456',
+    expectResult: '账号或密码错误',
+    type: 1
+  },
+  {
+    account: 'sawuhuw@givmail.com',
+    pwd: '123456',
+    expectResult: '🎉 🎉 🎉 登录成功！',
+    type: 0
+  }];
 
 type LoginTestDataUnit = {
   account: string,
@@ -64,48 +65,44 @@ const readExcel = async () => {
   return result;
 }
 
-beforeAll(async () => {
+beforeEach(async () => {
   // loginTestData = await readExcel();
   // await page.waitForTimeout(5000)
-  await page.goto('http://localhost:8001');
-  await page.waitForNavigation();
+  await page.goto('http://localhost:8001/user/login');
 });
 
 const testLogin = (item: LoginTestDataUnit, page: Page) => {
   const { account, pwd, expectResult, type } = item;
-  type === 0 ?
-    test('正确登录', async () => {
-      let toastText;
-      await loginFunc(page, account, pwd);
-      await page.waitForResponse(response => response.url().includes('/receipt/login/login') && response.status() === 200);
-      // const jsonRes = (await response.json()) as JsonResponse;
+  const text = type === 0 ? '账密正确' : '账密错误';
+  test(text, async () => {
+    let toastText;
+    await loginFunc(page, account, pwd);
+    await page.waitForResponse(response => response.url().includes('/receipt/login/login') && response.status() === 200);
+    // const jsonRes = (await response.json()) as JsonResponse;
+    if (type === 0)
       await page.waitForNavigation();
-      toastText = await page.$eval('div > .ant-message > div > .ant-message-notice > .ant-message-notice-content > div > span:nth-child(2)', el => (el as HTMLElement).innerText);
-      // expect to be“🎉 🎉 🎉 登录成功！”
-      expect(toastText).toMatch(expectResult);
-    })
-    :
-    test('密码错误登录', async () => {
-      let toastText;
-      await loginFunc(page, account, pwd);
-      await page.waitForResponse(response => response.url().includes('/receipt/login/login') && response.status() === 200);
-      // const jsonRes = (await response.json()) as JsonResponse;
-      toastText = await page.$eval('div > .ant-message > div > .ant-message-notice > .ant-message-notice-content > div > span:nth-child(2)', el => (el as HTMLElement).innerText);
-      expect(toastText).toMatch(expectResult);
-    });
+    toastText = await page.$eval('div > .ant-message > div > .ant-message-notice > .ant-message-notice-content > div > span:nth-child(2)', el => (el as HTMLElement).innerText);
+    // 账密正确与错误的登录结果不一样
+    await expect(toastText).toMatch(expectResult);
+  }, 10000);
 }
 
 
-describe.each(loginTestData)('($variable.account)测试登录', (item: LoginTestDataUnit) => {
+describe.each(loginTestData)('$variable.account测试登录', (item: LoginTestDataUnit) => {
   testLogin(item, page);
-},10000)
+})
 
-describe.skip('Onpay Login And Logout', () => {
+describe('Onpay Login And Logout', () => {
+  beforeAll(async () => {
+    await page.goto('http://localhost:8001/user/login');
+  });
+
   it('should be titled "OnPay"', async () => {
     await expect(page.title()).resolves.toMatch('OnPay');
   });
 
   it('正确退出', async () => {
+    await loginFunc(page,'faqob@dropjar.com', 'Xyc980830');
     await page.waitForSelector('span.ant-dropdown-trigger');
     await page.hover('span.ant-dropdown-trigger');
     await page.waitForSelector('span.ant-dropdown-menu-title-content > span.anticon-logout');
