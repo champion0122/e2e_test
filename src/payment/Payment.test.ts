43a4c13235811ef178cb0e7ed9a23bac16edb228
xueyunchen <yunchenxue30@gmail.com>
2022-08-01 15:20:03 +0800

    payment

--- src/payment/Payment.test.ts
+++ src/payment/Payment.test.ts
@@ -1,13 +1,64 @@
-import { loginFunc } from "../utils/login";
-import { ElementHandle, Page } from "puppeteer";
+import { ElementHandle, Page } from 'puppeteer';
+import { loginFunc } from '../utils/login';
 
-beforeEach(async () => {
-  await page.goto('http://localhost:8001');
-  await page.waitForNavigation();
-  await loginFunc(page);
-})
+type PaymentData = {
+  // 非货物贸易 0 货物贸易 1
+  paymentType: 0 | 1,
+  // 付款币种 USD: 0 CNH: 1
+  paymentCoin: 0 | 1,
+  // 交易类型 线下一般货物贸易 0 电商 1
+  tradeType?: 0 | 1,
+  // 付款账户类型 离岸账户: 0 在岸账户: 1
+  paymentAccountType: 0 | 1,
+  // 付款数量
+  paymentAmount?: string,
+  // 到账金额
+  paymentAmountTo?: string,
+  // 收款账户 从0开始
+  paymentAccount: number,
+  // 支付密码
+  pwd: string,
+  // 预期结果 0 成功 1 失败
+  expectResult: 0 | 1,
+  // 付款附言
+  postScript?: string,
+  // 操作备注
+  remark?: string,
+}
 
-const withdraw = async (page: Page) => {
+const paymentCoinMap = {
+  0: 'USD',
+  1: 'CNH'
+}
+
+const paymentTestData: PaymentData[] = [
+  {
+    paymentType: 1,
+    paymentCoin: 0,
+    paymentAccountType: 0,
+    paymentAmount: '10',
+    paymentAccount: 1,
+    pwd: '123456',
+    expectResult: 0,
+    postScript: 'testtesttesttesttesttesttesttesttesttesttestttesttesttest',
+    remark: 'testtesttesttesttesttesttesttesttesttesttest'
+  },
+  {
+    paymentType: 0,
+    paymentCoin: 1,
+    paymentAccountType: 0,
+    paymentAmount: '10',
+    paymentAccount: 1,
+    pwd: '123456',
+    expectResult: 1,
+    postScript: 'testtesttesttesttesttesttesttesttesttesttestttesttesttest',
+    remark: 'testtesttesttesttesttesttesttesttesttesttest'
+  },
+]
+
+const fiatPay = async (page: Page, paymentData: PaymentData) => {
+  const { paymentType, paymentCoin, tradeType, paymentAccountType, paymentAmount, paymentAmountTo, paymentAccount, pwd, expectResult, postScript, remark } = paymentData;
+
   await page.waitForSelector('.ant-pro-sider-menu')
 
   await page.waitForSelector('[title=付款管理]')
@@ -17,62 +68,91 @@ const withdraw = async (page: Page) => {
   await page.waitForTimeout(1000);
   await page.click('[title=付款]')
 
-  // await page.waitForNavigation();
+  await page.waitForSelector('#basic_type > label.ant-radio-wrapper.ant-radio-wrapper-in-form-item')
+  await page.click('#basic_type > label.ant-radio-wrapper.ant-radio-wrapper-in-form-item')
 
-  await page.waitForSelector('.ant-form-item-control-input-content > #basic_type > .ant-radio-wrapper:nth-child(2) > .ant-radio > .ant-radio-input')
-  await page.click('.ant-form-item-control-input-content > #basic_type > .ant-radio-wrapper:nth-child(2) > .ant-radio > .ant-radio-input')
+  // 根据paymentType选择付款类型
+  if (paymentType === 1) {
+    await page.keyboard.press('ArrowRight');
+    await page.waitForTimeout(1000);
+    await page.click('#basic_tradeType > label.ant-radio-wrapper.ant-radio-wrapper-in-form-item')
 
-  await page.waitForSelector('.ant-form-item-control-input-content > #basic_tradeType > .ant-radio-wrapper:nth-child(2) > .ant-radio > .ant-radio-input')
-  await page.click('.ant-form-item-control-input-content > #basic_tradeType > .ant-radio-wrapper:nth-child(2) > .ant-radio > .ant-radio-input')
+    if (tradeType === 1) {
+      await page.keyboard.press('ArrowRight');
+      await page.waitForTimeout(1000);
+    }
 
-  const uploaders: Array<ElementHandle> = await page.$$('.ant-upload > input');
-  for (let i = 0; i < uploaders.length - 1; i++) {
-    await uploaders[i].uploadFile('assets/lake.jpeg')
-    await page.waitForTimeout(1500)
+    const uploaders: Array<ElementHandle> = await page.$$('.ant-upload > input');
+    for (let i = 0; i < uploaders.length - 1; i++) {
+      await uploaders[i].uploadFile('assets/lake.jpeg')
+      await page.waitForTimeout(1500)
+    }
   }
 
-  await page.waitForSelector('label[for=basic_currency]')
-  await page.click('label[for=basic_currency]')
-  // 选中下拉框第二项
-  await page.keyboard.press('ArrowDown')
-  await page.keyboard.press('ArrowDown')
+  await page.waitForTimeout(5000);
+  await page.waitForSelector('#basic_currency')
+  await page.click('#basic_currency')
+  for (let i = 0; i < paymentCoin; i++) {
+    await page.keyboard.press('ArrowDown')
+  }
   await page.keyboard.press('Enter');
 
-  await page.waitForSelector('.ant-form-item-control-input-content > #basic_isOnshore > .ant-radio-wrapper:nth-child(2) > .ant-radio > .ant-radio-input')
-  await page.click('.ant-form-item-control-input-content > #basic_isOnshore > .ant-radio-wrapper:nth-child(2) > .ant-radio > .ant-radio-input')
 
-  await page.waitForSelector('#basic_amount')
+  await page.waitForSelector('#basic_isOnshore > label.ant-radio-wrapper.ant-radio-wrapper-in-form-item')
+  await page.click('#basic_isOnshore > label.ant-radio-wrapper.ant-radio-wrapper-in-form-item')
+
+  // 根据paymentAccountType选择付款账户类型
+  if (paymentAccountType === 1) {
+    await page.keyboard.press('ArrowRight');
+    await page.waitForTimeout(1000);
+  }
+
   await page.click('#basic_amount')
+  if (paymentAmount) {
+    await page.type('#basic_amount', paymentAmount)
+  } else {
+    await page.type('#basic_arriveAmount', paymentAmountTo)
+  }
 
-  await page.type('#basic_arriveAmount', '10')
+  await page.waitForSelector('#basic_bankId')
+  await page.click('#basic_bankId')
 
-  // await page.waitForSelector('#basic_account')
-  // await page.click('#basic_account')
-
-  // 选择账号下拉框第一项
-  await page.waitForSelector('label[for=basic_bankId]')
-  await page.click('label[for=basic_bankId]')
+  // 选择账号下拉框第i项
+  for (let i = 0; i < paymentAccount; i++) {
+    await page.keyboard.press('ArrowDown')
+  }
   await page.keyboard.press('Enter');
-  // await page.waitForSelector('.rc-virtual-list-holder > div > .rc-virtual-list-holder-inner > .app-select-option-pre:nth-child(2) > .ant-select-item-option-content')
-  // await page.click('.rc-virtual-list-holder > div > .rc-virtual-list-holder-inner > .app-select-option-pre:nth-child(2) > .ant-select-item-option-content')
 
-  await page.type('#basic_payPassword', '123456')
+  await page.type('#basic_password', pwd)
 
-  await page.type('#basic_toPayeePostscript', 'testtesttesttesttesttesttest')
+  await page.type('#basic_toPayeePostscript', postScript)
 
-  await page.type('#basic_operationNote', 'testtesttesttest')
+  await page.type('#basic_operationNote', remark)
 
-  await page.waitForSelector('.ant-row > .ant-col > .ant-form-item-control-input > .ant-form-item-control-input-content > .ant-btn-primary')
-  await page.click('.ant-row > .ant-col > .ant-form-item-control-input > .ant-form-item-control-input-content > .ant-btn-primary')
+  await page.waitForSelector('.ant-col > .ant-form-item-control-input > .ant-form-item-control-input-content > .ant-btn > span')
+  await page.click('.ant-col > .ant-form-item-control-input > .ant-form-item-control-input-content > .ant-btn > span')
 }
 
-
-describe('付款 test', () => {
-  it('付款 submit', async () => {
-    await withdraw(page);
-    // get '/receipt/account/fiatWithdrawToBank' response
-    const response = await page.waitForResponse(response => response.url().includes('/receipt/account/fiatPayToBank') && response.status() === 200);
-    const resJson: any = await response.json();
-    expect(resJson.success).toBe(true);
-  })
+beforeEach(async () => {
+  await page.goto('http://localhost:8001');
+  await page.waitForNavigation();
+  await loginFunc(page);
 })
+
+describe.each(paymentTestData)(`付款测试`, (item: PaymentData) => {
+  const { paymentCoin, expectResult, paymentAmount, paymentAmountTo } = item;
+  const isWithdrawSuccess = expectResult === 0;
+  const text = isWithdrawSuccess ? `${paymentCoinMap[paymentCoin]} 付款 ${paymentAmount ?? paymentAmountTo} 成功` : `${paymentCoinMap[paymentCoin]} 付款 ${paymentAmount ?? paymentAmountTo} 失败`;
+  test(text, async () => {
+    await fiatPay(page, item);
+
+    try{
+      const exchangeResult = await page.waitForResponse(response => response.url().includes('/receipt/account/fiatPayToBank') && response.status() === 200, {timeout: 3000});
+      const exchangeResultJson:any = await exchangeResult.json();
+
+      await expect(exchangeResultJson.success).toBe(isWithdrawSuccess);
+    }catch(e){
+      console.error(e)
+    }
+  }, 20000);
+});
--- src/withdraw/WithdrawDigital.test.ts
+++ src/withdraw/WithdrawDigital.test.ts
@@ -1,13 +1,65 @@
 import { loginFunc } from "../utils/login";
 import { Page } from "puppeteer";
 
+type WithdrawDigital = {
+  // USDT 0 USDC 1 CNHC 2
+  withdrawType: number,
+  // ERC20 0 TRC20 1
+  chainType: number,
+  withdrawAmount: string,
+  receiveAddress: string,
+  pwd: string,
+  expectResult: 0 | 1,
+}
+
+// number string Map
+type nsMap = {
+  [key: number]: string 
+}
+
+const withdrawCoinMap:nsMap = {
+  0: 'USDT',
+  1: 'USDC',
+  2: 'CNH'
+}
+
+const withdrawDigitalData: WithdrawDigital[] = [
+  // USDT 'TAaijyZPcMkW9uRJdx2Uxb3pASpJDbqth2' '123456'
+  {
+    withdrawType: 0,
+    chainType: 1,
+    withdrawAmount: '10',
+    receiveAddress: 'TAaijyZPcMkW9uRJdx2Uxb3pASpJDbqth2',
+    pwd: '123456',
+    expectResult: 0,
+  },
+  // USDC '0x2748aE2Baa06Ff00F2bdFfa9FADddC2106d79071' '123456'
+  {
+    withdrawType: 1,
+    chainType: 0,
+    withdrawAmount: '5',
+    receiveAddress: '0x2748aE2Baa06Ff00F2bdFfa9FADddC2106d79071',
+    pwd: '123456',
+    expectResult: 0,
+  },
+  {
+    withdrawType: 1,
+    chainType: 0,
+    withdrawAmount: '10',
+    receiveAddress: '0x2748aE2Baa06Ff00F2bdFfa9FADddC2106d79071',
+    pwd: '123459',
+    expectResult: 1,
+  },
+]
+
 beforeEach(async() => {
   await page.goto('http://localhost:8001');
   await page.waitForNavigation();
   await loginFunc(page);
 })
 
-const withdrawDigital = async(page: Page) => {
+const withdrawDigital = async(page: Page, data: WithdrawDigital) => {
+  const { withdrawType, chainType, withdrawAmount, receiveAddress, pwd } = data;
   await page.waitForSelector('.ant-pro-sider-menu')
 
   await page.waitForSelector('[title=提现管理]')
@@ -19,34 +71,49 @@ const withdrawDigital = async(page: Page) => {
 
   await page.waitForResponse(res => res.url().includes('/receipt/account/getAccountInfo') && res.status() === 200); 
 
+  await page.waitForTimeout(2000);
   await page.waitForSelector('#basic_fiat')
   await page.click('#basic_fiat')
+  for(let i = 0; i < withdrawType; i++) {
+    await page.keyboard.press('ArrowDown');
+  }
   await page.keyboard.press('Enter')
 
   await page.waitForResponse(res => res.url().includes('/receipt/account/getDigitalWithdrawConfig') && res.status() === 200); 
 
+  await page.waitForSelector('#basic_chainName > label')
+  await page.click('#basic_chainName > label')
+
+  // 根据withdrawAccountType选择提现账户类型
+  if (chainType === 1) {
+    await page.keyboard.press('ArrowRight');
+  }
+
   await page.waitForSelector('#basic_withdrawalNumber')
   await page.click('#basic_withdrawalNumber')
-  await page.keyboard.type('10')
+  await page.type('#basic_withdrawalNumber', withdrawAmount)
 
   await page.waitForSelector('#basic_address')
-  await page.click('#basic_address')
-  await page.keyboard.type('0xE6725AB01657246d7026Ffda1f853456a307F613')
+  await page.type('#basic_address',receiveAddress)
 
   await page.waitForSelector('#basic_password')
   await page.click('#basic_password')
-  await page.keyboard.type('123456')
+  await page.type('#basic_password',pwd)
 
   await page.waitForSelector('.ant-form-item-control-input-content > .app-btn-submit-lg')
   await page.click('.ant-form-item-control-input-content > .app-btn-submit-lg')
 }
 
-describe('数字货币提币test', () => {
-  it('数字货币提币 submit', async () => {
-    await withdrawDigital(page);
+describe.each(withdrawDigitalData)(`数字货币提币test`, (data) => {
+  const { withdrawType, withdrawAmount, expectResult } = data;
+  const isWithdrawSuccess = expectResult === 0;
+  const text = isWithdrawSuccess ? `${withdrawCoinMap[withdrawType]} 提现 ${withdrawAmount} 成功` : `${withdrawCoinMap[withdrawType]} 提现 ${withdrawAmount} 失败`;
+  it(text, async () => {
+    await withdrawDigital(page, data);
     // get '/receipt/account/fiatWithdrawToBank' response
     const response = await page.waitForResponse(response => response.url().includes('/receipt/account/digitalWithdraw') && response.status() === 200);
     const resJson:any = await response.json();
-    expect(resJson.success).toBe(true);
+    console.log(resJson)
+    expect(resJson.success).toBe(isWithdrawSuccess);
   })
 })
